e10ae0aaf32fc5370667be60aeea98f14226303e
Manteiga <rafael.manteiga@hotmail.com>
2023-04-24 04:39:50 -0300

    Update 341.hs

--- 341.hs
+++ 341.hs
@@ -1,7 +1,7 @@
 testandosortby :: (Ord n, Ord nn) => (n,nn) -> (n,nn) -> Ordering  
 testandosortby (n1,nn1) (n2,nn2)
-     | n1 < n2     = GT  
+     | n1 < n2     = LT  
      | n2 == n1    = EQ  
-     | otherwise = LT
+     | otherwise = GT
 
 sortBy testandosortby [(22445435, "Rafael"), (31122345, "Gabriel"), (21213333, "Júnior")]
